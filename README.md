8e2f52370dcbd5c9f3adea96e5ecbf0ca51fe41a
dakal-oleksandr <dakal.oleksandr@gmail.com>
2015-11-09 15:40:42 +0200

    Base cdn module

--- README.md
+++ README.md
@@ -1,25 +1,60 @@
 # Ember-cli-cdn
 
-This README outlines the details of collaborating on this Ember addon.
+Replacing links with CDN links, includes fallbacks, customization.
 
 ## Installation
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+To install as an Ember CLI addon:
 
-## Running
+`$ ember install ember-cli-cdn`
 
-* `ember server`
-* Visit your app at http://localhost:4200.
+### Why to use
 
-## Running Tests
+This plugin takes content of files and replace matched links by patterns to CDN links with a lot 
+of options to customize output. 
+Additionally it can exclude from concatenated vendor files and move them as separate link 
+to template container.
 
-* `ember test`
-* `ember test --server`
+### Usage
 
-## Building
+In index.html include container:
 
-* `ember build`
+`{{content-for 'cdn'}}`
 
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+In Brocfile.js or ember-cli-build.js:
+
+```
+var app = new EmberApp({
+  cdn: {
+    enabled: true,
+    // Options
+    files: [{
+      file: '**/jquery.*js',
+      cdn: 'cdnjs:jquery'
+    }]
+  }
+});
+```
+
+In result jQuery will be excluded from vendor.js file and included to html as 
+separate tags that loads from CDN.
+
+```
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js" data-original="assets/jquery-7decc060e1d33ab4c0ea42ed03d3d797.js"></script>
+
+<script>if(!(window.jQuery)) cdnizerLoad("assets/jquery-7decc060e1d33ab4c0ea42ed03d3d797.js");</script>
+```
+
+## Options
+
+### enabled
+
+Define if plugin is enabled. By default plugin enabled with environment `production`.
+
+### files
+
+See the cdnizer [files options](https://github.com/OverZealous/cdnizer#optionsfiles).
+
+## License
+
+MIT © Dakal Oleksandr
--- index.js
+++ index.js
@@ -1,6 +1,119 @@
 /* jshint node: true */
 'use strict';
 
+// Modules injection
+var path = require('path');
+var _ = require('lodash');
+var minimatch = require('minimatch');
+
+// Broccoli plugins
+var Cdnizer = require('broccoli-cdnizer');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+var unwatchedTree = require('broccoli-unwatched-tree');
+
+// Addon
 module.exports = {
-  name: 'ember-cli-cdn'
+  name: 'ember-cli-cdn',
+  included: function (app) {
+    // Invoke parent method
+    this._super.included.apply(this, arguments);
+
+    // Local variables
+    var usrDefinedOpts = app.options.cdn || {},
+      cdn;
+
+    cdn = {
+      app: app,
+      // Project root
+      root: app.project.root,
+      // Define if addon enabled
+      enabled: typeof usrDefinedOpts.enabled === 'boolean' ?
+        usrDefinedOpts.enabled : app.env === 'production',
+      // Cdn files options
+      cdnize: usrDefinedOpts.files || [],
+      // Path to assets
+      assets: path.dirname(app.options.outputPaths.vendor.js)
+        .replace(new RegExp('^' + path.sep), ''),
+      // Files that included in vendor's bundle files
+      bunledFiles: [].concat(app.vendorStaticStyles,
+        app.legacyFilesToAppend),
+      // Files that will be included to template
+      contentForLinks: [],
+      contentForHTML: ''
+    };
+
+    if (cdn.enabled) {
+      // Iterate vendor files in order build defines
+      _.forEach(cdn.bunledFiles, function (filePath) {
+        _.forEach(cdn.cdnize, function (opt) {
+          // Match cdn glob
+          if (minimatch(filePath, opt.file || '')) {
+            cdn.contentForLinks.push(filePath);
+          }
+        });
+      });
+
+      // Exclude files from bundles
+      app.vendorStaticStyles = _.difference(app.vendorStaticStyles,
+        cdn.contentForLinks);
+      app.legacyFilesToAppend = _.difference(app.legacyFilesToAppend,
+        cdn.contentForLinks);
+    }
+
+    this.cdn = cdn;
+  },
+  preBuild: function () {
+    var cdn = this.cdn;
+
+    if (cdn.enabled) {
+      this.contentForHTML = _.reduce(cdn.contentForLinks,
+        function (acc, filePath) {
+          var link = path.join(cdn.assets, path.basename(filePath));
+
+          return acc + renderTag(link);
+        }, '', this);
+    }
+
+    function renderTag(link) {
+      var extension = path.extname(link),
+        tag = '';
+
+      if (extension === '.js') {
+        tag = '<scr' + 'ipt src="' + link + '" data-original="' + link + '"></scr' + 'ipt>';
+      } else if (extension === '.css') {
+        tag = '<link rel="stylesheet" href="' + link + '" data-original="' + link + '"/>';
+      }
+
+      return tag;
+    }
+  },
+  contentFor: function (name) {
+    if (name === 'cdn') {
+      return this.contentForHTML;
+    }
+  },
+  postprocessTree: function (type, tree) {
+    var cdn = this.cdn,
+      cdnizeTree, assetsTree;
+
+    if(cdn.enabled && type === 'all'){
+      cdnizeTree = new Cdnizer(tree, {
+        files: cdn.cdnize
+      });
+
+      assetsTree = new Funnel(unwatchedTree(cdn.root), {
+        include: cdn.contentForLinks,
+        getDestinationPath: function (relativePath) {
+          return path.join(cdn.assets, path.basename(relativePath));
+        }
+      });
+
+      tree = new MergeTrees([tree, cdnizeTree, assetsTree], {
+        overwrite: true
+      });
+    }
+
+    return tree;
+  }
 };
